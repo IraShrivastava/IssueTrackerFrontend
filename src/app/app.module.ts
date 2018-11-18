ee36d31204c1d1e21379e483aa7e27a5940b83d9
zealous-zayn <imzayn93@gmail.com>
2018-11-18 21:02:36 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule, Component, } from '@angular/core';
 import { AppComponent } from './app.component';
+import{RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+import { UserModule } from './user/user.module';
+import { CookieService } from 'ngx-cookie-service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardModule } from './dashboard.module';
+import{ ToastrModule } from 'ngx-toastr'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    UserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent, pathMatch:'full'},
+      {path:"", redirectTo:'login', pathMatch:'full'},
+      {path:'*', component:LoginComponent},
+      {path:'**', component:LoginComponent}
+    ]),
+    DashboardModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
